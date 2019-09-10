8b50ed371c0eeac8b4089317e34bca02163597c6
bxker <jake.luke.baker@gmail.com>
2019-09-10 11:57:43 -0500

    initial commit

---
+++ server/index.js
@@ -0,0 +1,92 @@
+const express = require("express");
+const session = require('express-session')
+require("dotenv").config();
+
+const app = express();
+
+app.use(express.json());
+
+const movies = [];
+let id = 0;
+
+app.use(session({
+    resave: false,
+    saveUninitialized: true,
+    secret: process.env.SESSION_SECRET,
+    cookie: {
+        maxAge: 604800000 //or 1000 * 60 * 60 * 24 * 7
+    }
+}))
+
+app.post("/auth/login", (req, res)=> {
+    req.session.username = req.body.username;
+    req.session.password = req.body.password;
+})
+
+
+//MIDDLEWARE 
+function authenticateUser(req, res, next) {
+    // if(req.body.username === process.env.POST_USERNAME && req.body.password === process.env.POST_PASSWORD) {
+    //     next();
+    // } else {
+    //     res.status(403).json({
+    //         message: "Invalid Username or Password"
+    //     })
+    // }
+    if(req.session.username !== undefined && req.session.password !== undefined){
+        if(req.session.username === process.env.POST_USERNAME && req.session.password === process.env.POST_PASSWORD){
+            next();
+        }else {
+            res.status(403).json({
+                message: "Invalid Username or Password"
+            })
+        }
+    }else {
+        res.status(500).json('Please log in')
+    }
+}
+
+// applies to all endpoints below
+app.use(authenticateUser);
+
+app.get("/api/movies", (req, res) => {
+    res.status(200).json(movies);
+})
+
+app.post("/api/movies", (req, res) => {
+    const {title, duration, genre} = req.body;
+    movies.push({
+        title,
+        duration,
+        genre,
+        id
+    });
+    id++;
+    res.status(200).json(movies);
+})
+
+app.put("/api/movies/:id", (req, res) => {
+    const {title, duration, genre} = req.body;
+    const {id} = req.params;
+    const index = movies.findIndex((val) => {
+        return val.id == id;
+    })
+    movies[index] = {
+        title,
+        duration,
+        genre,
+        id
+    }
+    res.status(200).json(movies);
+})
+
+app.delete("/api/movies/:id", (req, res) => {
+    const {id} = req.params;
+    const index = movies.findIndex((val) => {
+        return val.id == id;
+    })
+    movies.splice(index, 1);
+    res.status(200).json(movies);
+})
+
+app.listen(5050, () => console.log(`Listening on Port 5050`));
